c99ff73249d3397ee8909c5869550f5128c29d12
Aditya shah <shah.aditya.204@gmail.com>
2024-02-23 16:23:03 -0500

    midterm

---
+++ src/Aditya_Design.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aditya
+ */
+public class Aditya_Design {
+// My favourite Subjects Are Maths,Physics,Chemistry,Astronomy .
+// I also like Python and Java coding. 
+// I Also like to Use GitHub to build me projects.
+    
+}
